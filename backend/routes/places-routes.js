a93659438405cfc508b1c849c917105fb96a77be
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-23 20:12:06 +0200

    Adding Place-Specific Routes

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const placesControllers = require('../controllers/places-controllers');
+const router = express.Router();
+
+router.get('/:pid', placesControllers.getPlaceById);
+
+module.exports = router;
