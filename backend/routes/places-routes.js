08938221f90b5f341643bc745ea282e74830ae8c
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-27 19:22:53 +0300

    Deleting Places

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -10,4 +10,6 @@ router.post('/', placesControllers.createPlace);
 
 router.patch('/:pid', placesControllers.updatePlace);
 
+router.delete('/:pid', placesControllers.deletePlace);
+
 module.exports = router;
